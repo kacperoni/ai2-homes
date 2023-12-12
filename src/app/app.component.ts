e66bd179a6be2721cad566b9591b178777918766
Kacper Kubacki <kacper.kubacki87@gmail.com>
2023-12-12 19:30:04 +0100

    :tada: Lab f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab-f';
+
+  constructor(private authService: AuthService) {
+  }
+
+  public canAccessUsers(): boolean{
+    return this.authService.isAdmin();
+  }
+
+  public canAccessItems(): boolean{
+    return this.authService.isAuthentiacted();
+  }
+
+  public getUsername(): string | null{
+    return this.authService.getUsername();
+  }
+
+  public canLogout(): boolean{
+    return this.authService.isAuthentiacted();
+  }
+
+  public logout(): void{
+    this.authService.logout();
+  }
 }
